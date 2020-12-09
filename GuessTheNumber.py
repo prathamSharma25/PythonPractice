be70e30b1c6610f72ecabd835461b16f0cdb1e76
Pratham Sharma <prathams2425@gmail.com>
2020-12-09 12:44:26 +0530

    Update GuessTheNumber.py
    Solved errors in original file:
    1. Used function random.randinit instead of random.randint
    1. Displaying success message even for incorrect guess
    2. Displaying error message for correct guess ("Your number was Smaller than Random Number")
    
    Added:
    1. 5 lives for user to guess number
    2. General comments to explain logic

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,21 +1,39 @@
 import random
 
-def my_function(input_nos):
-	if input_nos != r:
-		print("Great Job-- You guessed the number correctly")
-	elif input_nos>r:
-		print("Your number was Greater than Random Number")
-	else:
-		print("Your number was Smaller than Random Number")
-		
-		
+def check(guess):
+    '''
+    Function to check whether user's guess is correct and display appropriate message.
+    Function returns 1 (True) if user has guessed correctly. Otherwise, the function returns 0 (False).
+    '''
+    if guess==random_num:
+        print("Great Job -- You guessed the number correctly!")
+        return 1
+    else:
+        if guess>random_num:
+            print("Your number was Greater than Random Number.")
+        else:
+            print("Your number was Smaller than Random Number.")
+        return 0
+
+# generate random integer between 0 and 20 using randint function
+random_num = random.randint(0, 20)
 print("Computer has selected a Random Number between 0 and 20. Guess the number computer has selected!!")
-user_rand=input("Make a guess: " )
-r=random.randinit(0,20)
-print("Guessed Number is: "+user_rand)
-while user_rand != r:
-	my_function(int(user_rand))
-	if int(user_rand) == int(r)
-		break
-	else:
-		user_rand=input("Make a guess: ")
+# set lives equal to 5
+lives = 5
+# let user guess 5 times
+while lives>0:
+    print("\nLives remaining: ", lives)
+    guess = int(input("Make a guess: "))
+    # if user guesses correctly, check(guess) will return 1 and if will be executed
+    if check(guess):
+        break
+    # if user guesses incorrectly, else will be executed
+    else:
+        # decrement lives by 1
+        lives = lives - 1
+        # if user has lives remaining, try again
+        if lives:
+            print("Try again.")
+# if all lives are used
+if not lives:
+    print("The random number selected was: ", random_num)
