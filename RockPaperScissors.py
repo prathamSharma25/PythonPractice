6c9a2a3b5b857de0e07eb1f59ee52c5ef664df17
Pratham Sharma <prathams2425@gmail.com>
2020-12-11 11:22:32 +0530

    Add files via upload

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -20,30 +20,28 @@ def result(pair):
     User's choice is the first element in the tuple 'pair' and Computer's choice is the second.
     '''
     # Display user's and computer's choice
-    print("User's Choice: ", pair[0])
-    print("Computer's Choice:", pair[1])
+    print("User's Choice: {}".format(pair[0]))
+    print("Computer's Choice: {}".format(pair[1]))
     # If any draw condition is satisfied
     if pair in draw:
-        print("Its a draw. Both selected ", pair[0])
+        print("Its a draw. Both selected {}.".format(pair[0]))
     # If win condition for User is satisfied
     elif pair in win:
-        print("User WON the game as {} beats {}".format(pair[0], pair[1]))
+        print("User WON the game as {} beats {}.".format(pair[0], pair[1]))
     # Else Computer wins
     else:
-        print("Computer WON the game as {} beats {}".format(pair[1], pair[0]))
+        print("Computer WON the game as {} beats {}.".format(pair[1], pair[0]))
         
-# Dictionary to pair tools with a number
-tools = {0: 'Rock', 1:'Paper', 2:'Scissors'}
 # User's and Computer's choice is denoted as a tuple => (users_choice, computer_choice)
 # Condition for a draw between User and Computer
-draw = [(0, 0), (1, 1), (2, 2)]
+draw = [('Rock', 'Rock'), ('Paper', 'Paper'), ('Scissors', 'Scissors')]
 # Condition for a win for User
-win = [(1, 0), (2, 1), (0, 2)]
+win = [('Paper', 'Rock'), ('Scissors', 'Paper'), ('Rock', 'Scissors')]
 
 # Read and check User's input
 user = input("Make a Guess: ")
 check_user_input(user)
-# Computer selects a tool at random from the dictionary 'tools'
-computer = tools[random.randint(0, 2)]
+# Computer selects a tool at random from the list of tools
+computer = random.choice(['Rock', 'Paper', 'Scissors'])
 # Get result for the game; User's and Computer's choice is passed as a tuple => (user, computer)
 result((user, computer))
